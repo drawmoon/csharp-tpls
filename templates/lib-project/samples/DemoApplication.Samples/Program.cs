f159840ca723310d2c783b229281cebc0bd05f83
drsh <41671372+drawmoon@users.noreply.github.com>
2022-09-28 15:06:34 +0800

    first commit

---
+++ templates/lib-project/samples/DemoApplication.Samples/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace DemoApplication.Samples
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"Hello, World! {string.Join(',', args)}");
+        }
+    }
+}
